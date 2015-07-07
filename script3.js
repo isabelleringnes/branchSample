4b3586a905f397469a863b86f6a869e78fbd945b
Isabelle Ringnes <isabelleringnes@gmail.com>
2015-07-07 13:53:50 -0400

    update jquery

--- script3.js
+++ script3.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
 	$(".button").click(function() {
 		$("#perfect").toggle();
-		$("p").hide();
+		$("p").toggle();
 		$("h1").slideUp(2000);
      	});
 	$("h2").fadeIn(4000);
+		});
 
+$(".headline").click(function() {
+$(".headline").animate( {
+	opacity: 0.25,
+	width: "70%",
+	
+} , 2000 );
+
+
+$(".bottom").click(function() {
+	$("body").show();
+});
 		
 });
 
 
+function myFunction() {
+    alert("What is wrong with you?!");
+
+}
